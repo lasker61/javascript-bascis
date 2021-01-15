dc304ee48f5c6fbf04fd0d092f8102c64576791a
lasker61 <laskernahid2@gmail.com>
2021-01-16 01:47:49 +0600

    updated

---
+++ string.js
@@ -0,0 +1,6 @@
+var promise = "i promise you that i will become a web developer";
+console.log(promise.toLowerCase()); //built in function.it makes the strinlower case
+
+var paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec quo modo conveniant, non sane intellego";
+
+console.log(paragraph.toUpperCase());
---
+++ varnaming.js
@@ -0,0 +1,6 @@
+var properCamelCase;
+var studlyCase;
+var myFirstName;
+properCamelCase ="this is the way we should write variables name";
+console.log(properCamelCase);
+console.log(studlyCase);
