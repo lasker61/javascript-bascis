ebe6208987836ab67cc13fcf56285072c9c2505f
lasker61 <laskernahid2@gmail.com>
2021-01-16 00:40:07 +0600

    updated

---
+++ variable.js
@@ -0,0 +1,14 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+
+var firstName ="nahid"; //this is a in-line comment
+var lastName ="lasker";
+console.log(firstName);
+console.log(lastName);
+/*this is a 
+multiline comment
+*/
+var myName = "Nahid Lasker";
+var myName = "hossain Nahid"; //as we can seee myName will be updated with hossain Nahid
+const pi =3.1416;// const can not be changed
+let ourName = "Programming hero" //let works within the scope!
