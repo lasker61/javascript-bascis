11c6c99e2f35e3757766bfeb43c64e46a63563df
lasker61 <laskernahid2@gmail.com>
2021-01-16 11:51:01 +0600

    updated

---
+++ operators.js
@@ -0,0 +1,19 @@
+var a = 6;
+var b = 7;
+var c =45;
+
+a = a + 456; // a +=456;
+b += 89;
+c += 56;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//same goes for sub,mul,div,rem
+
+a -= 62;
+console.log(a);
+
+b *= 2;
+console.log(b);
