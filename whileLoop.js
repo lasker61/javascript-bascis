8741500c1066aeb934cc42087e7351d4b9a5b4fb
lasker61 <laskernahid2@gmail.com>
2021-01-17 01:13:13 +0600

    updated

--- whileLoop.js
+++ whileLoop.js
@@ -1,1 +1,12 @@
-console.log("while loop");
+var number = 0;
+while(number < 15 ){
+    // console.log(number);
+    number++;
+
+}
+
+var num = 8;
+while (num > 0) {
+    console.log(num);
+    num--;    
+}
