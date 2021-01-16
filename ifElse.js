323c57632c6196fb21abc48291b60b6a7c52ac15
lasker61 <laskernahid2@gmail.com>
2021-01-16 22:15:45 +0600

    updated

---
+++ ifElse.js
@@ -0,0 +1,64 @@
+var biscuitprice = 12;
+if(biscuitprice < 10){
+    console.log("i will eat biscuits");
+}
+else{
+    console.log("i will not eat biscuits");
+}
+//
+var number = 0;
+if (number > 0) {
+    console.log("you have entered positive number");
+}
+else if (number < 0) {
+    console.log("you have entered negative number");
+}
+else{
+    console.log("you have entered 0");
+}
+//
+var age = 17;
+if (age >= 18) {
+    console.log("you are eligible for voting");
+}
+else{
+    console.log('you are not eligible for voting');
+}
+//
+var a = 6;
+var b = 7;
+if (a == b) {
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b");
+}
+//
+var number = 4;
+if (number % 2 ==0) {
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
+//
+var c;
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+    console.log("alphabet");
+}
+else{
+    console.log("not alphabet");
+}
+//
+
+var jobPaiso = true;
+var savingAmount = 200000;
+if(jobPaiso == true && savingAmount < 200000){
+    console.log("saving amount baraw,then biye koiro");
+}
+else if (jobPaiso == true && savingAmount >= 200000) {
+    console.log("asho patri khuji");
+}
+else{
+    console.log("kopale biya nai");
+}
