a71cf09601114b6f5eeb26b181b7ce39b7a03091
lasker61 <laskernahid2@gmail.com>
2021-01-16 11:40:11 +0600

    updated

---
+++ math.js
@@ -0,0 +1,40 @@
+//addition
+
+var price1 = 45;
+var price2 =95;
+var total = price1 + price2;
+console.log(total);
+//subtruction
+
+var sub = price2 -price1;
+console.log(sub);
+
+//multiplying
+
+var mul = price1 * price2;
+console.log(mul);
+
+//division
+var div = price2 / price1;
+console.log(div);
+
+var reminder = price2 % price1;
+console.log(reminder);
+
+
+//a2+2ab+b2
+var a = 3;
+var b = 4;
+var result = a**2 + 2*a*b + b**2;
+console.log(result);
+
+//increamenting variables
+
+myVar = 6;
+myVar ++;
+console.log(myVar); // ormyVar=myVar+1;
+
+//decreamenting variables
+
+myVar --;
+console.log(myVar);
