9e1f9d435f8e6ac67ea53ddf2ade60037c61bf2d
lasker61 <laskernahid2@gmail.com>
2021-01-16 19:50:17 +0600

    updated

---
+++ builtInFunctions.js
@@ -0,0 +1,25 @@
+//abs() function shows any numbers to its absolute value!
+var number = -8376;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+// round() function shows only round figures
+var price = 4.4665;
+var roundPrice = Math.round(price);
+console.log(roundPrice);
+
+//ceil() function shows the upper round figures only
+var score = 7.35;
+var ceilScore = Math.ceil(score);
+console.log(ceilScore);
+
+// floor() function shows thw lower round figure only
+var cgpa = 3.8765;
+var cgpaFloor = Math.floor(cgpa);
+console.log(cgpaFloor);
+
+//random number
+
+var randomNumber = 8;
+var calculateRandomNumber = Math.random(randomNumber);
+console.log(calculateRandomNumber);
