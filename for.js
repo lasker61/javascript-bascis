8da9cbc6384baf4a72c97784c293bef814f7cd39
lasker61 <laskernahid2@gmail.com>
2021-01-17 01:46:40 +0600

    updated

---
+++ for.js
@@ -0,0 +1,11 @@
+var num=10;
+var i;
+for (i = 1; i <= 10 ; i++){
+    console.log(i);
+}
+
+var nums = [45,45,6,3,875,5,23];
+for (var i = 0;  i<nums.length; i++) {
+    var element = nums[i];
+    console.log(element);
+}
