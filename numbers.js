40b823991b9769f25ab83af70b647ce751e72e6f
lasker61 <laskernahid2@gmail.com>
2021-01-16 10:56:43 +0600

    updated

---
+++ numbers.js
@@ -0,0 +1,13 @@
+var number1 = 15; //integer
+var number2 = 25.5 //float
+var number3 =number1 + number2;
+console.log(number3);
+
+var num1 = 10;
+var num2 ='2.6';
+var num3 = '11.22';
+num3 =parseFloat(num3);
+num2 = parseInt(num2); //parse function converts string into integer or float
+num2 = parseFloat(num2);
+console.log(num1 + num2);
+console.log(num2 +num3);
