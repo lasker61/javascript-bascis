e23b6e93869d328a67d3c83a2538cbefa1386acb
lasker61 <laskernahid2@gmail.com>
2021-01-17 11:41:29 +0600

    updated

---
+++ function.js
@@ -0,0 +1,24 @@
+function sayLoveYou(){
+    console.log("jaan o baby ");
+    console.log("licchi picchi jinchi");
+}
+sayLoveYou();
+
+
+
+function doubleIt(num){
+    var result = num * 2;
+    return result;
+}
+ var first = doubleIt(5);
+ var second =doubleIt(34);
+ var total = first +second;
+ console.log(total);
+
+
+ function adder(a,b){
+     var answer = a + b;
+     return answer;
+ }
+ var func = adder(4,5);
+ console.log(func);
