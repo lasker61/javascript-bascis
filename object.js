ba63bd0aab907b712d80458b05252334b81072e7
lasker61 <laskernahid2@gmail.com>
2021-01-17 11:56:59 +0600

    updated

---
+++ object.js
@@ -0,0 +1,16 @@
+var student ={
+    name:'abir',
+    id:21234,
+    mobile:019873573
+};
+var student2 ={
+    name:'mahia mahi',
+    id:74637,
+    mobile:0947563950
+};
+var id = student2.id;
+student2.id =45678;
+student2.cgpa =3.13;
+console.log(student ,student2);
+console.log(student2.name);
+console.log(student.mobile);
