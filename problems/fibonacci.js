7b638be98b5658b044981eda2772d27fd7c4ed9e
lasker61 <laskernahid2@gmail.com>
2021-01-19 22:48:04 +0600

    updated

---
+++ problems/fibonacci.js
@@ -0,0 +1,24 @@
+var fibo = [0,1];
+//fibo[2] =fibo[2-1]+fibo[2-2]
+//fibo[3] =fibo[3-1]+fibo[3-2]
+//fibo[4] =fibo[4-1]+fibo[4-2]
+//fibo[5] =fibo[5-1]+fibo[5-2]
+//fibo[n] =fibo[n-1]+fibo[n-2] 
+
+for( i= 2 ; i <= 12 ; i++){
+    fibo[i] = fibo[i-1] + fibo[i-2];
+}
+console.log(fibo);
+
+
+//using function
+
+function findFibonacci(number){
+    var fibo = [0,1];
+    for( i= 2 ; i <= number ; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+var result = findFibonacci(15);
+console.log(result);
