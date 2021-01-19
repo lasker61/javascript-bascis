96948fd52559ae58833a0701ed45ab705302aef8
lasker61 <laskernahid2@gmail.com>
2021-01-19 11:37:11 +0600

    updated

---
+++ problems/inchToFeet.js
@@ -0,0 +1,12 @@
+
+ function inchToFeet(inch){
+     var feet;
+     feet= inch/12;
+     return feet;
+ }
+ var dada = inchToFeet(176);
+ console.log(dada);
+
+ var nana = inchToFeet(120);
+ console.log(nana);
+ 
