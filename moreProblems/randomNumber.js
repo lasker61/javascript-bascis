e724077da0ac2fdcac0d8e871e0231ced5a4b8cd
lasker61 <laskernahid2@gmail.com>
2021-01-20 18:33:37 +0600

    updated

---
+++ moreProblems/randomNumber.js
@@ -0,0 +1,19 @@
+var number = 5.853;
+var result1 = Math.round(number);
+var result2 = Math.floor(number);
+var result3 = Math.ceil(number);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+
+
+//lets play a game
+
+for( i = 1 ; i <= 10 ; i++ ){
+    var randomNumber =Math.random() *6;
+    var dice = Math.round(randomNumber);
+    console.log(dice);
+}
+
+
+
