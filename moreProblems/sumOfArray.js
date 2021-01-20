ae278db3819a566d1833bb3a2f0b29322ca6c6a2
lasker61 <laskernahid2@gmail.com>
2021-01-20 20:51:40 +0600

    updated

---
+++ moreProblems/sumOfArray.js
@@ -0,0 +1,7 @@
+var numbers = [56,45,356,23,546,432,355,245];
+var sum = 0;
+for(var i = 0 ; i < numbers.length ; i++){
+    var element = numbers[i];
+    sum = sum + element;
+}
+console.log("sum of all numbers is: ",sum);
